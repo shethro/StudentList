b1eeb47c9de792a8fb9b10799fe69f0991bd9b18
ronak sheth <ronak.sheth@sheridancollege.ca>
2022-05-17 14:13:43 -0400

    added private  attribute address i student class and getter and setter

--- src/student_spr22_tue/Student.java
+++ src/student_spr22_tue/Student.java
@@ -4,6 +4,15 @@ package student_spr22_tue;
 public class Student {
     private int studentNum;
     private String studentName;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public Student(int studentNum, String studentName) {
         this.studentNum = studentNum;
