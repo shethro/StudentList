e3df3aea7d10065a03cf5b77b8f2b6034c622945
ronak sheth <ronak.sheth@sheridancollege.ca>
2022-05-24 14:06:47 -0400

    add part time branch

---
+++ src/student_spr22_tue/PartTimeStudent.java
@@ -0,0 +1,18 @@
+
+package student_spr22_tue;
+
+
+public class PartTimeStudent extends Student {
+    private int numOfCourses;
+
+   
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    
+    
+}
